4edd7ab6a07043950bd8aa162ae07f6b3198c38e
futantan <luckytantanfu@gmail.com>
2015-07-17 23:35:20 +0800

    Styled the search bar and added the icon

---
+++ StoreSearch/SearchResultCell.swift
@@ -0,0 +1,31 @@
+//
+//  SearchResultCell.swift
+//  StoreSearch
+//
+//  Created by luckytantanfu on 15/7/17.
+//  Copyright (c) 2015年 futantan. All rights reserved.
+//
+
+import UIKit
+
+class SearchResultCell: UITableViewCell {
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var artistNameLabel: UILabel!
+    @IBOutlet weak var artworkImageView: UIImageView!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    
+        let selectedView = UIView(frame: CGRect.zeroRect)
+        selectedView.backgroundColor = UIColor(red: 20/255, green: 160/255, blue: 160/255, alpha: 0.5)
+        selectedBackgroundView = selectedView
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
