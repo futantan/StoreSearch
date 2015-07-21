2810f41b43dd8dc7644d05ca78bbccf75fe3094d
futantan <luckytantanfu@gmail.com>
2015-07-21 20:19:00 +0800

    add animation

--- StoreSearch/DetailViewController.swift
+++ StoreSearch/DetailViewController.swift
@@ -91,8 +91,17 @@ class DetailViewController: UIViewController {
 }
 
 extension DetailViewController: UIViewControllerTransitioningDelegate {
+    
     func presentationControllerForPresentedViewController(presented: UIViewController, presentingViewController presenting: UIViewController!, sourceViewController source: UIViewController) -> UIPresentationController? {
         return DimmingPresentationController(presentedViewController: presented, presentingViewController: presenting)
+    }
+    
+    func animationControllerForPresentedController(presented: UIViewController, presentingController presenting: UIViewController, sourceController source: UIViewController) -> UIViewControllerAnimatedTransitioning? {
+        return BounceAnimationController()
+    }
+    
+    func animationControllerForDismissedController(dismissed: UIViewController) -> UIViewControllerAnimatedTransitioning? {
+        return SlideOutAnimationController()
     }
 }
 
